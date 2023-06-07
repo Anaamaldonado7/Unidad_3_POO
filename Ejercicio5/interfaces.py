5ec6b5319fc0b6323c20bb09c1da90fd70355bf0
Anaamaldonado7 <132727743+Anaamaldonado7@users.noreply.github.com>
2023-06-07 12:37:54 -0300

    Delete Ejercicio5 directory

--- Ejercicio5/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope.interface import Interface
-
-class elemento(Interface):
-    def insertarElemento(i,elem):
-        pass
-    def agregarElemento(elem):
-        pass
-    def mostrarElemento(i):
-        pass 
