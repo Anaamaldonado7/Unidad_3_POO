9ea0fb94e53bb18883ab4948fb4268ccfa1581eb
Anaamaldonado7 <132727743+Anaamaldonado7@users.noreply.github.com>
2023-06-06 22:14:38 -0300

    Add files via upload

---
+++ Ejercicio5/interfaces.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+
+class elemento(Interface):
+    def insertarElemento(i,elem):
+        pass
+    def agregarElemento(elem):
+        pass
+    def mostrarElemento(i):
+        pass 
